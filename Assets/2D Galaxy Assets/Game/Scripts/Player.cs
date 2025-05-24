f995121cd4fdf40ad45be7ffb5a833966ed1784c
Romelg18 <rggualoto10@gmail.com>
2025-05-24 09:09:00 -0500

    Implementacion de Disparo triple

--- Assets/2D Galaxy Assets/Game/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Game/Scripts/Player.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public bool canTripleShot=false;
+    
+
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+
+    [SerializeField]
     private float _fireRate = 0.25f;
     private float _canFire = 0.0f;
 
+
+
     [SerializeField]
     private float _speed = 5.5f;
+    
     
 
     void Start()
@@ -23,21 +33,34 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
-        Shoot();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+
+        }
+        
         
     }
 
 
     private void Shoot()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+       
+
+        if (Time.time > _canFire)
         {
-            if (Time.time > _canFire)
+
+            if (canTripleShot == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+            }
+            else
             {
                 Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
-                _canFire = Time.time + _fireRate;
             }
-
+            
+            _canFire = Time.time + _fireRate;
         }
     }
 
@@ -68,6 +91,17 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(9.5f, transform.position.y, 0);
         }
+    }
+
+    public void TripleShotPowerupOn()
+    {
+        canTripleShot=true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+    public IEnumerator TripleShotPowerDownRoutine() 
+    {
+        yield return new WaitForSeconds(5.0f);
+        canTripleShot = false;
     }
 }
 
